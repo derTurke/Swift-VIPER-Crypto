09c53ed5e396562f1f25fa8a6d3bfe594ed76635
GÜRHAN YUVARLAK <76452852+derTurke@users.noreply.github.com>
2022-08-24 02:19:16 +0300

    first commit

---
+++ Swift-VIPER-Crypto/home-module/Entity.swift
@@ -0,0 +1,13 @@
+//
+//  Entity.swift
+//  Swift-VIPER-Crypto
+//
+//  Created by GÜRHAN YUVARLAK on 24.08.2022.
+//
+
+import Foundation
+
+struct Crypto: Decodable {
+    let currency: String
+    let price: String
+}
---
+++ Swift-VIPER-Crypto/home-module/Router.swift
@@ -0,0 +1,41 @@
+//
+//  Router.swift
+//  Swift-VIPER-Crypto
+//
+//  Created by GÜRHAN YUVARLAK on 24.08.2022.
+//
+
+import Foundation
+import UIKit
+
+// Class, protocol
+// EntryPoint
+typealias EntryPoint = AnyView & UIViewController
+
+
+protocol AnyRouter {
+    var entry: EntryPoint? {get}
+    static func startExecution() -> AnyRouter
+}
+
+class CryptoRouter: AnyRouter {
+    var entry: EntryPoint?
+    
+    static func startExecution() -> AnyRouter {
+        let router = CryptoRouter()
+        var view: AnyView = CryptoViewController()
+        var presenter: AnyPresenter = CryptoPresenter()
+        var interactor: AnyInteractor = CryptoInteractor()
+        view.presenter = presenter
+        
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        
+        interactor.presenter = presenter
+        
+        router.entry = view as? EntryPoint
+        
+        return router
+    }
+}
